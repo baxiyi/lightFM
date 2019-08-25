920b1eae0e9e94cc32fbc64fff126402887d4eef
baxiyi <mzw5810@gmail.com>
2019-08-25 21:16:23 +0800

    Updates

---
+++ precache-manifest.c22855d68761797c063295ae2a59b386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a75dc58a6de1a808adf890fc3af84aa7",
+    "url": "/lightFM/index.html"
+  },
+  {
+    "revision": "1dc5d56f7abca3230abd",
+    "url": "/lightFM/static/css/main.bd7ad021.chunk.css"
+  },
+  {
+    "revision": "763a175a7c582c97ef8f",
+    "url": "/lightFM/static/js/2.0725e970.chunk.js"
+  },
+  {
+    "revision": "1dc5d56f7abca3230abd",
+    "url": "/lightFM/static/js/main.956ca6a1.chunk.js"
+  },
+  {
+    "revision": "514eea3dfe7c85838dfb",
+    "url": "/lightFM/static/js/runtime~main.d4adc99e.js"
+  },
+  {
+    "revision": "dd6a5bf3b64088716514ec736c64caa0",
+    "url": "/lightFM/static/media/bg.dd6a5bf3.jpg"
+  }
+]);
